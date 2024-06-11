72bbb8d57f271f9c43ec3502665ecb682d2b56d1
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-06-11 21:07:46 +0330

    chore: add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import type { ReactNode } from "react";
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import { Inter } from "next/font/google";
 
 import { NextUIContext } from "@saba-idea/context/next-ui.context";
@@ -8,6 +10,31 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const myFont = localFont({
+  src: [
+    {
+      path: "../fonts/Shabnam/Shabnam-Thin-FD.woff2",
+      weight: "100",
+    },
+    {
+      path: "../fonts/Shabnam/Shabnam-Light-FD.woff2",
+      weight: "300",
+    },
+    {
+      path: "../fonts/Shabnam/Shabnam-FD.woff2",
+      weight: "400",
+    },
+    {
+      path: "../fonts/Shabnam/Shabnam-Medium-FD.woff2",
+      weight: "500",
+    },
+    {
+      path: "../fonts/Shabnam/Shabnam-Bold-FD.woff2",
+      weight: "700",
+    },
+  ],
+});
+
 export const metadata: Metadata = {
   title: "تماشای آنلاین فیلم و سریال",
 };
@@ -15,11 +42,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="fa" dir="rtl">
+      <body className={myFont.className}>
         <NextUIContext>
           <ProgressContext>{children}</ProgressContext>
         </NextUIContext>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { Card, CardBody } from "@nextui-org/react";
-
 export default function Home() {
   return (
     <div>
       <h1>Saba Idea</h1>
-      <Card>
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-      </Card>
+      <p>تست فیلیمو</p>
     </div>
   );
 }
