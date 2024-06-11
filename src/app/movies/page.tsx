a605513815886e84d6ba4f7d4ddb1f8b7f428ee6
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-06-12 01:45:22 +0330

    fix: build

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,51 +1,13 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
-import { useMemo } from "react";
+import { Suspense } from "react";
 
-import GenreFilter from "@saba-idea/components/genre-filter";
-import MovieCard from "@saba-idea/components/movie-card";
-import RateFilter from "@saba-idea/components/rete-filter";
-import moviesData from "@saba-idea/data/index.json";
-import { filterAndSortMovies } from "@saba-idea/helper/movies";
-import { Movie } from "@saba-idea/models";
+import Movies from "@saba-idea/components/movies";
 
 export default function MoviesPage() {
-  const searchParams = useSearchParams();
-
-  const selectedRateMode = searchParams.get("order_dir");
-  const selectedGenre = searchParams.get("genre") ?? "";
-  const selectedGenreList = selectedGenre.split(",");
-
-  const selectedMovies = useMemo(() => {
-    return filterAndSortMovies(
-      moviesData,
-      selectedGenreList.filter((item) => item.length),
-      (selectedRateMode as "asc" | "desc") ?? "asc"
-    );
-  }, [selectedRateMode, selectedGenreList]);
-
   return (
-    <div>
-      <div className="flex items-center gap-4 justify-center">
-        <GenreFilter />
-        <RateFilter />
-      </div>
-      <div className="text-gray-200 text-xs font-bold mt-12 mb-4">
-        لیست تمامی فیلم و سریال‌ها
-      </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6  gap-4">
-        {selectedMovies.map((movie: Movie) => {
-          return (
-            <MovieCard
-              key={movie.id}
-              movieName={movie.movie_title}
-              movieImage={movie.pic.movie_img_s}
-              movieRate={movie.rate_avrage}
-            />
-          );
-        })}
-      </div>
-    </div>
+    <Suspense>
+      <Movies />
+    </Suspense>
   );
 }
