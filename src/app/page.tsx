e098c75a24e78d50689e96924684fb90730f0147
Alireza Ettehadi <alireza.ettehadi9595@gmail.com>
2024-06-11 21:14:15 +0330

    feat: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
+import { Button } from "@nextui-org/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Saba Idea</h1>
-      <p>تست فیلیمو</p>
-    </div>
+    <main className="w-full flex items-center justify-center h-screen">
+      <div className="flex items-center flex-col gap-4">
+        <h1 className="text-yellow-400">به وبسایت فیلیمو خوش‌آمدید!</h1>
+        <Button as={Link} variant="flat" href="/movies" color="warning">
+          Go back home
+        </Button>
+      </div>
+    </main>
   );
 }
